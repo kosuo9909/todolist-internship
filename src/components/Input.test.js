b9697c029bb3360620dfb320ffa45bc8b342c946
Kostadin Stanev <kostadin.stanev@p3-group.com>
2023-08-03 10:09:42 +0300

    writing unit tests for Input.js

---
+++ src/components/Input.test.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+import { fireEvent, getByText, render } from '@testing-library/react';
+import Input from './Input';
+import { describe, it, jest } from '@jest/globals';
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+jest.mock('uuid', () => ({
+  v4: () => '1',
+}));
+
+describe('Input Component', () => {
+  it('renders without crashing', () => {
+    const mockAddItemHandler = jest.fn();
+    render(<Input addItemHandler={mockAddItemHandler} />);
+  });
+
+  it('handles user input for name and price', () => {
+    const mockAddItemHandler = jest.fn();
+    const { getByPlaceholderText } = render(
+      <Input addItemHandler={mockAddItemHandler} />
+    );
+    const nameInput = getByPlaceholderText('Item Name');
+    const priceInput = getByPlaceholderText('Item Price');
+
+    fireEvent.change(nameInput, { target: { value: 'Test Item' } });
+    fireEvent.change(priceInput, { target: { value: '10' } });
+
+    expect(nameInput.value).toBe('Test Item');
+    expect(priceInput.value).toBe('10');
+  });
+
+  it('calls addItemHandler function on button click', () => {
+    const mockAddItemHandler = jest.fn();
+
+    const { getByPlaceholderText, getByText } = render(
+      <Input addItemHandler={mockAddItemHandler} />
+    );
+    const nameInput = getByPlaceholderText('Item Name');
+    const priceInput = getByPlaceholderText('Item Price');
+    const addButton = getByText(/Add Item/i);
+
+    fireEvent.change(nameInput, { target: { value: 'Test Item' } });
+    fireEvent.change(priceInput, { target: { value: '10' } });
+
+    fireEvent.click(addButton);
+
+    expect(mockAddItemHandler).toHaveBeenCalledWith({
+      name: 'Test Item',
+      price: '10',
+      quantity: 1,
+      id: '1',
+    });
+  });
+});
